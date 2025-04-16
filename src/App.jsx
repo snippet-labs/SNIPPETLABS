5afec8affbf541f4af92c4094a49b46e907014ee
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-04-16 11:16:05 +0530

    setup(complete): add tailwindcss, zustand; base site ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 const App = () => {
-  return <div>SNIPPETLABS</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-slate-900 text-green-600 text-3xl md:text-4xl lg:text-8xl">
+      SNIPPETLABS
+    </div>
+  );
 };
 export default App;
